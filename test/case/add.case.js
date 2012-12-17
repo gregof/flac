f642fa4c347a53ce1c1bf69e69bb3e440ea6c0e2
Fedor Golubev <fgolubev@yandex-team.ru>
2012-12-17 20:08:42 +0400

    remove debug log; new tests

---
+++ test/case/add.case.js
@@ -0,0 +1,77 @@
+//in
+var abc = require('abc');
+var indexer = require(tc.fixPath('../../lib/index.js'));
+
+abc.async.sequence(
+    [
+        function (callback) {
+            tc.execConsole([
+                'mkdir tmp/a',
+                'echo \'{"name":"a"}\' > tmp/a/module.json',
+                'echo \'{"name":"pack"}\' > tmp/package.json'
+            ].join(';'), callback)
+        },
+        function (callback) {
+            indexer.scan('tmp', function (objects) {
+                tc.out(JSON.stringify(tc.sort(objects), null, '  '))
+                callback();
+            })
+        },
+        function (callback) {
+            tc.execConsole([
+                'mkdir tmp/b',
+                'echo \'{"name":"b"}\' > tmp/b/module.json'
+            ].join(';'), callback)
+        },
+        function (callback) {
+            indexer.scan('tmp', function (objects) {
+                tc.out(JSON.stringify(tc.sort(objects), null, '  '))
+                callback();
+            })
+        }
+
+    ],
+    function () {
+        tc.finish();
+    }
+);
+//out
+[
+  {
+    "type": "module",
+    "file": "a/module.json",
+    "desc": {
+      "name": "a"
+    }
+  },
+  {
+    "type": "package",
+    "file": "package.json",
+    "desc": {
+      "name": "pack"
+    }
+  }
+]
+[
+  {
+    "type": "module",
+    "file": "a/module.json",
+    "desc": {
+      "name": "a"
+    }
+  },
+  {
+    "type": "module",
+    "file": "b/module.json",
+    "desc": {
+      "name": "b"
+    }
+  },
+  {
+    "type": "package",
+    "file": "package.json",
+    "desc": {
+      "name": "pack"
+    }
+  }
+]
--- test/case/tc.conf.js
+++ test/case/tc.conf.js
@@ -1,1 +1,24 @@
-{exec:'async'}
+{
+    exec:'async',
+    beforeEach: function (tc, callback) {
+        tc.execConsole = function exec (command, callback) {
+            require('child_process').exec(command, function (err, stdout, stderr) {
+                console.log(command);
+                if (err) {
+                    tc.out(command + ':' + err);
+                }
+                callback();
+            });
+        };
+        tc.sort = function (arr) {
+            arr.sort(function (a, b) {
+                return a.desc.name > b.desc.name ? 1 : -1
+            })
+            return arr;
+        }
+        tc.execConsole('mkdir tmp', callback);
+    },
+    afterEach: function (tc, callback) {
+        tc.execConsole('rm -rf tmp', callback);
+    }
+}
